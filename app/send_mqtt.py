567029850562a7c46958c5c94eef8a308cd2f146
Patrik <128815730+patriklts@users.noreply.github.com>
2025-01-20 17:39:33 +0100

    Revert "feat: add frequency topic and dynamic interval adjustment for MQTT messages"
    This reverts commit e293509c3718af1c7f51a772fd9767fcf63c10bf.

--- app/send_mqtt.py
+++ app/send_mqtt.py
@@ -9,7 +9,6 @@ DEFAULT_BROKER = "localhost"
 DEFAULT_PORT = 1883
 DEFAULT_TOPIC_TEMPERATURES = "iot/devices/temperatures"
 DEFAULT_TOPIC_HUMIDITY = "iot/devices/humidity"
-DEFAULT_TOPIC_FREQUENCY = "iot/devices/frequency"
 DEFAULT_USERNAME = None
 DEFAULT_PASSWORD = None
 SEND_MQTT_INTERVAL = 10
@@ -19,7 +18,6 @@ BROKER = os.getenv("BROKER_IP", DEFAULT_BROKER)
 PORT = int(os.getenv("BROKER_PORT", DEFAULT_PORT))
 TOPIC_TEMPERATURES = os.getenv("TOPIC_TEMPERATURES", DEFAULT_TOPIC_TEMPERATURES)
 TOPIC_HUMIDITY = os.getenv("TOPIC_HUMIDITY", DEFAULT_TOPIC_HUMIDITY)
-TOPIC_FREQUENCY = os.getenv("TOPIC_FREQUENCY", DEFAULT_TOPIC_FREQUENCY)
 MQTT_USERNAME = os.getenv("MQTT_USERNAME", DEFAULT_USERNAME)
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", DEFAULT_PASSWORD)
 CLIENT_ID = os.getenv("CLIENT_ID", "dht22-sensor")
@@ -31,27 +29,19 @@ client = mqtt.Client()
 if MQTT_USERNAME and MQTT_PASSWORD:
     client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
-# Callback-Funktionen
+# Callback-Funktionen für Debugging
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Verbunden mit dem Broker")
-        client.subscribe(TOPIC_FREQUENCY) # Subscribe to the frequency topic, if the frequency should be changed
     else:
         print(f"Verbindung fehlgeschlagen. Code: {rc}")
 
 def on_publish(client, userdata, mid):
     print(f"Nachricht mit ID {mid} veröffentlicht")
 
-def on_message(client, userdata, message):
-    print(f"Nachricht empfangen: {message.payload.decode()}")
-    if message.topic == TOPIC_FREQUENCY:
-        global SEND_MQTT_INTERVAL
-        SEND_MQTT_INTERVAL = int(message.payload.decode())
-        print(f"Send interval changed to {SEND_MQTT_INTERVAL} seconds")
 
 client.on_connect = on_connect
 client.on_publish = on_publish
-client.on_message = on_message
 
 # Verbinden mit dem Broker
 client.connect(BROKER, PORT, keepalive=60)
@@ -81,7 +71,6 @@ def send_mqtt(data):
     })
     client.publish(TOPIC_TEMPERATURES, temperature_payload)
     print(f"Temperaturen gesendet: {temperature_payload}")
-
 
 if __name__ == "__main__":
     client.loop_start()  # Startet die MQTT-Netzwerkkommunikation im Hintergrund
