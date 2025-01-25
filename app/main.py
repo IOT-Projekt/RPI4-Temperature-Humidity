28b43f9d4b766e63460e457bde9b01d18fcf4fab
Patrik <128815730+patriklts@users.noreply.github.com>
2025-01-25 20:34:35 +0100

    added comments and changed comments from de to en

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 # sending interval in seconds, default is 10 seconds
 send_message_interval = 10
 
-# Umgebungsvariablen
+# Constants based on environment variables
 BROKER = os.getenv("BROKER_IP", "localhost")
 PORT = int(os.getenv("BROKER_PORT", 1883))
 TOPIC_TEMPERATURES = os.getenv("TOPIC_TEMPERATURES", "iot/devices/temperatures")
@@ -27,63 +27,63 @@ MQTT_USERNAME = os.getenv("MQTT_USERNAME", None)
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", None)
 CLIENT_ID = os.getenv("CLIENT_ID", "dht22-sensor")
 
-# Initialisiere MQTT-Client
+# Initialize the MQTT client
 client = mqtt.Client()
 
-# Authentifizierung hinzufügen
+# If username and password are set, use them for authentication
 if MQTT_USERNAME and MQTT_PASSWORD:
     client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
 
-# Callback-Funktionen für Connection logging
+# Callback function for connection logging
 def on_connect(client, userdata, flags, rc):
-    # Wenn die Verbindung erfolgreich ist, wird das Topic Frequenz abonniert
+    # If the connection was successful, subscribe to the frequency topic
     if rc == 0:
         logging.info("Verbunden mit dem Broker")
         client.subscribe(TOPIC_FREQUENCY)
-        logging.info(f"Abonniere {TOPIC_FREQUENCY}")  # Info Meldung loggen
+        logging.info(f"Abonniere {TOPIC_FREQUENCY}")  
     else:
         logging.error(f"Verbindung fehlgeschlagen. Code: {rc}")
 
 
-# Callback-Funktionen für Publish logging
+# Callback function for message logging
 def on_publish(client, userdata, mid):
     logging.info(f"Nachricht mit ID {mid} veröffentlicht")
 
 
-# Callback-Funktionen für Message logging
+# Callback function for message handling
 def on_message(client, userdata, message):
     logging.info(f"Nachricht empfangen: {message.payload.decode()}")
 
-    # Falls es sich um eine Frequenznachricht handelt, wird die Sendefrequenz aktualisiert
+    # If it is a frequency message, update the send_message_interval
     if message.topic == TOPIC_FREQUENCY:
-        # Lock setzen, um sicherzustellen, dass die Sendefrequenz nicht gleichzeitg im main Thread gelesen wird
+        # Set lock to ensure that the send frequency is not changed/read at the same time in the other thread
         with lock:
             global send_message_interval
 
-            # Frequenzwert aus der Nachricht extrahieren und Variable updaten
+            # Get frequency from payload and update the interval
             payload = json.loads(message.payload.decode())["payload"]
             send_message_interval = int(payload["frequency"])
 
             logging.info(f"Send interval changed to {send_message_interval} seconds")
 
 
-# Callback Funktionen setzen
+# Set the callback functions
 client.on_connect = on_connect
 client.on_publish = on_publish
 client.on_message = on_message
 
-# Verbinden mit dem Broker
+# Connect to the broker
 client.connect(BROKER, PORT, keepalive=60)
 
 
 def send_mqtt(data):
-    """Sendet Humdity, Temperatur und Zeitstempel an den MQTT-Broker."""
-    # Wenn keine Daten vorhanden sind, wird die Funktion beendet
+    """ Send humidity, temperature, and timestamp to the MQTT broker."""
+    # If no data was provided, return without sending
     if data is None:
         return
 
-    # Humdity Payload erstellen und an MQTT Broker senden
+    # Create humidity payload and send it to the MQTT broker
     humidity_payload = json.dumps(
         {
             "source": "mqtt",
@@ -95,7 +95,7 @@ def send_mqtt(data):
     client.publish(TOPIC_HUMIDITY, humidity_payload)
     logging.info(f"Feuchtigkeit gesendet: {humidity_payload}")
 
-    # Temperature Payload erstellen und an MQTT Broker senden
+    # Create temperature payload and send it to the MQTT broker
     temperature_payload = json.dumps(
         {
             "source": "mqtt",
@@ -109,15 +109,15 @@ def send_mqtt(data):
 
 
 if __name__ == "__main__":
-    # Startet die MQTT-Loop in einem eigenen Thread. Nachrichten zur Frequenzänderung werden somit in einem eigenen Thread verarbeitet
+    # start the MQTT client loop in a separate thread. Therefore messages with a new frequency can be received while sending data
     client.loop_start()
 
-    # Sensordaten in einer Endlosschleife einlesen, dem Broker senden und nach einer bestimmten Pause wiederholen
+    # Read sensor data and send it to the MQTT broker
     while True:
         sensor_data = read_sensor()
-        if sensor_data:  # Sicherstellen, dass auch Sensorwerte vorhanden sind
+        if sensor_data:  # ensure that sensor data is not None
             send_mqtt(sensor_data)
 
-        # Lock setzen, um sicherzustellen, dass die Sendefrequenz nicht gleichzeitg im anderen Thread geändert wird
+        # Set lock to ensure that the send frequency is not changed/read at the same time in the other thread
         with lock:
             time.sleep(send_message_interval)
--- app/read_sensor.py
+++ app/read_sensor.py
@@ -1,9 +1,12 @@
 import time
 import board
 import adafruit_dht
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def read_sensor():
-    """Liest Temperatur und Feuchtigkeit vom DHT22-Sensor."""
+    """ Reads temperature and humidity from the DHT22 sensor. Returns the values as a dictionary."""
     dhtDevice = adafruit_dht.DHT22(board.D2, use_pulseio=False)
     
     try:
@@ -15,8 +18,8 @@ def read_sensor():
             "timestamp": time.time()
         }
     except RuntimeError as error:
-        # Fehler beim Lesen des Sensors
-        print(f"Fehler beim Lesen des Sensors: {error.args[0]}")
+        # if an error occurs, the error message should be logged
+        logging.error(f"Fehler beim Lesen des Sensors: {error.args[0]}")
         return None
     except Exception as error:
         dhtDevice.exit()
